1e1fcc5900069fd9ba90a302a5ee99a8c50167ce
Mitul Pancholi <mitul.pancholi@live.com>
2019-05-25 17:42:09 +0530

    Done

--- controllers/user.js
+++ controllers/user.js
@@ -5,5 +5,10 @@ module.exports.first = function(req, res) {
 
 //USER SECOND API
 module.exports.second = function(req, res) {
-    return res.send(req.body)
+    return res.send('user second api called')
+};
+
+//USER MITUL API
+module.exports.mitul = function(req, res) {
+    return res.send({NAME: 'Mitul',EMAIL: 'mitul.pancholi@live.com',PHONE: +919624480764})
 };
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,8 @@ var controllers = require('../controllers/user');
 //GET ROUTES
 router.get('/first', controllers.first);
 router.get('/second', controllers.second);
+router.get('/mitul', controllers.mitul);
+
 
 //EXPORT ROUTER
 module.exports = router;
